ffd5cc59d6ab013413092d9611a8c645ba83be6d
TMVH <4forcestmvh@gmail.com>
2026-08-05 01:50:37 +0530

    Implement music play/pause functionality
    Added functionality to control background music playback.

--- script.js
+++ script.js
@@ -7,18 +7,31 @@ function copyScript(script){
 }
 
 const music = document.getElementById("bgMusic");
+
+music.volume = 0.25; // Change to 0.3, 0.4, 0.5 if you want it louder
+
 const musicBtn = document.getElementById("musicBtn");
 
 let playing = false;
 
 musicBtn.onclick = function () {
+
     if (!playing) {
+
         music.play();
+
         musicBtn.innerHTML = "⏸ Pause Music";
+
         playing = true;
+
     } else {
+
         music.pause();
+
         musicBtn.innerHTML = "🎵 Play Music";
+
         playing = false;
+
     }
+
 };
