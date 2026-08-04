0a850a2e5615011a761c7c2a625a7e9ef645f551
TMVH <4forcestmvh@gmail.com>
2026-08-05 01:39:50 +0530

    Add music playback functionality to script.js

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ function copyScript(script){
     alert("✅ Script copied successfully!");
 
 }
+
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
+
+let playing = false;
+
+musicBtn.onclick = function () {
+    if (!playing) {
+        music.play();
+        musicBtn.innerHTML = "⏸ Pause Music";
+        playing = true;
+    } else {
+        music.pause();
+        musicBtn.innerHTML = "🎵 Play Music";
+        playing = false;
+    }
+};
